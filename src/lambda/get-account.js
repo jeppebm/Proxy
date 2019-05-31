a0714539716a4405843cb6616d64acc0a0d32661
jeppebm <jbm@lassox.com>
2019-05-31 16:49:16 +0200

    - fix

--- src/lambda/get-account.js
+++ src/lambda/get-account.js
@@ -1,11 +1,12 @@
 import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
-  return fetch(event.queryStringParameters.url, { headers: { "Accept": "application/xml" } })
-    // .then(response => response.text())
-    // .then(data => ({
-    //   statusCode: 200,
-    //   body: data
-    // }))
-    // .catch(error => ({ statusCode: 422, body: String(error) }));
+  return fetch(event.queryStringParameters.url, {})
+    .then(response => response.text())
+    .then(data => ({
+      statusCode: 200,
+      body: data,
+      header: { 'Content-Type': event.contentType || 'text/xml' }
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
 };
